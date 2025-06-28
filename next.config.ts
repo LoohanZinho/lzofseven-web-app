eec1a0f89d7106e5e40c8916ac4c7dd4f677ae30
Lohan Santos <lohansantosborges@gmail.com>
2025-06-28 01:02:07 +0000

    Initial commit of Next.js project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Remova 'output: 'export',' se estiver aqui!
   typescript: {
     ignoreBuildErrors: true,
   },
